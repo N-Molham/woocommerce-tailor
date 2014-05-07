11c32cb703e3ca67cec33c7f84a73b9182a483b2
nabeelmolham <nabeelmolham@localhost>
2014-05-07 15:46:38 +0200

    account details sectiosn

--- wp-content/plugins/woocommerce-tailor/includes/classes/account-updates.php
+++ wp-content/plugins/woocommerce-tailor/includes/classes/account-updates.php
@@ -13,19 +13,105 @@ if ( ! defined( 'ABSPATH' ) ) {
 class WC_Tailor_Account_Updates
 {
 	/**
+	 * Account details sections
+	 *
+	 * @var array
+	 */
+	protected $account_details_sections;
+
+	/**
 	 * Account details fields
 	 *
 	 * @var array
 	 */
-	protected $account_details;
+	protected $account_details_fields;
+
+	/**
+	 * List of fields to be handled by WooCommerce by default
+	 * 
+	 * @var array
+	 */
+	var $handled_fields;
+
+	/**
+	 * User class properties ( buit-in )
+	 * 
+	 * @var array
+	 */
+	var $user_class_props;
+
+	/**
+	 * Input field default arguments
+	 * 
+	 * @var array
+	 */
+	var $field_defaults;
 
 	/**
 	 * Constructor
 	 */
 	public function __construct()
 	{
+		// set sections
+		$this->account_details_sections = apply_filters( 'woocommerce_tailor_account_details_sections', array (
+				'personal_info' => array ( 
+						'title' => __( 'Your Personal Details', WCT_DOMAIN ),
+						'description' => __( 'Our website makes ordering easy for you. We remember your measurements for next time you login. This is handy when you simply can\'t live without a few more finely crafted My Bespoke Tailor custom shirts.', WCT_DOMAIN ),
+				),
+		) );
+
 		// set fields info
-		$this->account_details = array ( 
+		$this->account_details_fields = apply_filters( 'woocommerce_tailor_account_details_fields', array ( 
+				'account_first_name' => array ( 
+						'label' => __( 'First name', WCT_DOMAIN ),
+						'meta_key' => 'first_name',
+						'input' => 'text',
+						'input_class' => 'input-text',
+						'wrapper_class' => 'form-row form-row-first',
+						'data_type' => 'text',
+						'required' => true,
+						'section' => 'personal_info',
+				),
+				'account_last_name' => array ( 
+						'label' => __( 'Last name', WCT_DOMAIN ),
+						'meta_key' => 'last_name',
+						'input' => 'text',
+						'input_class' => 'input-text',
+						'wrapper_class' => 'form-row form-row-last',
+						'data_type' => 'text',
+						'required' => true,
+						'section' => 'personal_info',
+				),
+				'account_email' => array ( 
+						'label' => __( 'Email address', WCT_DOMAIN ),
+						'meta_key' => 'user_email',
+						'input' => 'email',
+						'input_class' => 'input-text',
+						'wrapper_class' => 'form-row form-row-wide',
+						'data_type' => 'email',
+						'required' => true,
+						'section' => 'personal_info',
+				),
+				'password_1' => array ( 
+						'label' => __( 'Password (leave blank to leave unchanged)', WCT_DOMAIN ),
+						'meta_key' => '',
+						'input' => 'password',
+						'input_class' => 'input-text',
+						'wrapper_class' => 'form-row form-row-first',
+						'data_type' => 'text',
+						'required' => false,
+						'section' => 'personal_info',
+				),
+				'password_2' => array ( 
+						'label' => __( 'Confirm new password', WCT_DOMAIN ),
+						'meta_key' => '',
+						'input' => 'password',
+						'input_class' => 'input-text',
+						'wrapper_class' => 'form-row form-row-last',
+						'data_type' => 'text',
+						'required' => false,
+						'section' => 'personal_info',
+				),
 				'account_phone' => array ( 
 						'label' => __( 'Telephone Number', WCT_DOMAIN ),
 						'meta_key' => 'phone',
@@ -34,7 +120,7 @@ class WC_Tailor_Account_Updates
 						'wrapper_class' => 'form-row form-row-first',
 						'data_type' => 'text',
 						'required' => true,
-						'section' => 'general',
+						'section' => 'personal_info',
 				),
 				'account_gender' => array ( 
 						'label' => __( 'Gender', WCT_DOMAIN ),
@@ -48,9 +134,9 @@ class WC_Tailor_Account_Updates
 								'female' => __( 'Female', WCT_DOMAIN ),
 						 ),
 						'required' => true,
-						'section' => 'general',
+						'section' => 'personal_info',
 				),
-		);
+		) );
 		/*$measurements		= array( cm inches
 				"Chest",
 				"Waist",
@@ -82,8 +168,6 @@ class WC_Tailor_Account_Updates
  		both
  			Height cm   Weight kg
 		 */
-		// apply filter
-		$this->account_details = apply_filters( 'woocommerce_tailor_account_details', $this->account_details );
 
 		// Redirect new customer to edit account page
 		add_filter( 'woocommerce_registration_redirect', 'wc_customer_edit_account_url' );
@@ -99,6 +183,31 @@ class WC_Tailor_Account_Updates
 
 		// additional fields render
 		add_action( 'woocommerce_tailor_account_fields', array( &$this, 'render_account_details' ) );
+
+		// handled fields
+		$this->handled_fields = array ( 
+				'account_first_name',
+				'account_last_name',
+				'account_email',
+				'password_1',
+				'password_2',
+		);
+
+		// WP_User properties
+		$this->user_class_props = apply_filters( 'woocommerce_tailor_user_class_properties', array( 'first_name', 'last_name', 'user_email' ) );
+
+		// field arguments default values
+		$this->field_defaults = apply_filters( 'woocommerce_tailor_fields_defaults', array (
+				'label' => '',
+				'meta_key' => '',
+				'input' => 'text',
+				'input_class' => 'input-text',
+				'wrapper_class' => 'form-row form-row-wide',
+				'data_type' => 'text',
+				'required' => false,
+				'values' => array(),
+				'value' => '',
+		) );
 	}
 
 	/**
@@ -112,8 +221,12 @@ class WC_Tailor_Account_Updates
 	public function save_account_details_errors( $errors, $update, $user )
 	{
 		// loop fields
-		foreach ( $this->account_details as $field_name => $field_args )
+		foreach ( $this->account_details_fields as $field_name => $field_args )
 		{
+			// skip already handled fields
+			if ( in_array( $field_name, $this->handled_fields ) )
+				continue;
+
 			// sanitizing value
 			$value = wc_clean( filter_input( INPUT_POST, $field_name ) );
 
@@ -154,30 +267,41 @@ class WC_Tailor_Account_Updates
 		if ( !$user )
 			$user = wp_get_current_user();
 
-		// field arguments default values
-		$defaults = apply_filters( 'woocommerce_tailor_fields_defaults', array ( 
-				'label' => '',
-				'meta_key' => '',
-				'input' => 'text',
-				'input_class' => 'input-text',
-				'wrapper_class' => 'form-row form-row-wide',
-				'data_type' => 'text',
-				'required' => false,
-				'values' => array(),
-				'value' => '',
-		) );
-
 		// output holder
 		$output = '';
+		$handled_sections = array();
 
-		// loop fields
-		foreach ( $this->account_details as $field_name => $field_args )
+		// fields loop
+		foreach ( $this->account_details_fields as $field_name => $field_args )
 		{
-			// get value
-			$field_args['value'] = get_user_meta( $user->ID, $field_args['meta_key'], true );
+			// section handler
+			$field_section = $field_args['section'];
+			if ( !in_array( $field_section, $handled_sections ) && isset( $this->account_details_sections[$field_section] ) )
+			{
+				// section title
+				$output .= '<h3>'. $this->account_details_sections[$field_section]['title'] .'</h3>';
+
+				// section descriptions
+				$output .= '<p>'. $this->account_details_sections[$field_section]['description'] .'</p>';
+
+				// set as handled
+				$handled_sections[] = $field_section;
+			}
+
+			// field value
+			if ( in_array( $field_args['meta_key'], $this->user_class_props ) && isset( $user->$field_args['meta_key'] ) )
+			{
+				// from class property
+				$value = $user->$field_args['meta_key'];
+			}
+			else
+			{
+				// from meta
+				$value = empty( $field_args['meta_key'] ) ? '' : get_user_meta( $user->ID, $field_args['meta_key'], true );
+			}
 
 			// parse args
-			$field_args = apply_filters( 'woocommerce_tailor_account_field_args', wp_parse_args( $field_args, $defaults ), $field_name, $user );
+			$field_args = apply_filters( 'woocommerce_tailor_account_field_args', wp_parse_args( $field_args, $this->field_defaults ), $field_name, $user );
 
 			// wrapper
 			$output .= '<p class="'. $field_args['wrapper_class'] .'">';
@@ -189,14 +313,16 @@ class WC_Tailor_Account_Updates
 			switch ( $field_args['input'] )
 			{
 				case 'text':
-					$output .= '<input type="text" class="'. $field_args['input_class'] .'" name="'. $field_name .'" id="'. $field_name .'" value="'. esc_attr( $field_args['value'] ) .'" />';
+				case 'email':
+				case 'password':
+					$output .= '<input type="'. $field_args['input'] .'" class="'. $field_args['input_class'] .'" name="'. $field_name .'" id="'. $field_name .'" value="'. esc_attr( $value ) .'" />';
 					break;
 
 				case 'radio':
 					// loop values
 					foreach ( $field_args['values'] as $option_value => $option_label )
 					{
-						$output .= '<label><input type="radio" class="'. $field_args['input_class'] .'" name="'. $field_name .'" value="'. $option_value .'"'. checked( $field_args['value'], $option_value, false ) .'/> '. $option_label .'</label>';
+						$output .= '<label><input type="radio" class="'. $field_args['input_class'] .'" name="'. $field_name .'" value="'. $option_value .'"'. checked( $value, $option_value, false ) .'/> '. $option_label .'</label>';
 					}
 					break;
 			}
@@ -218,7 +344,7 @@ class WC_Tailor_Account_Updates
 	 */
 	public function get_account_details()
 	{
-		return $this->account_details;
+		return $this->account_details_fields;
 	}
 }
 
--- wp-content/plugins/woocommerce-tailor/templates/myaccount/form-edit-account.php
+++ wp-content/plugins/woocommerce-tailor/templates/myaccount/form-edit-account.php
@@ -16,29 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <form action="" method="post">
 
-	<h3><?php _e( 'Your Personal Details', BRETHEON_CHILD_DOMAIN ); ?></h3>
-
-	<p class="form-row form-row-first">
-		<label for="account_first_name"><?php _e( 'First name', BRETHEON_CHILD_DOMAIN ); ?> <span class="required">*</span></label>
-		<input type="text" class="input-text" name="account_first_name" id="account_first_name" value="<?php esc_attr_e( $user->first_name ); ?>" />
-	</p>
-	<p class="form-row form-row-last">
-		<label for="account_last_name"><?php _e( 'Last name', BRETHEON_CHILD_DOMAIN ); ?> <span class="required">*</span></label>
-		<input type="text" class="input-text" name="account_last_name" id="account_last_name" value="<?php esc_attr_e( $user->last_name ); ?>" />
-	</p>
-	<p class="form-row form-row-wide">
-		<label for="account_email"><?php _e( 'Email address', BRETHEON_CHILD_DOMAIN ); ?> <span class="required">*</span></label>
-		<input type="email" class="input-text" name="account_email" id="account_email" value="<?php esc_attr_e( $user->user_email ); ?>" />
-	</p>
-	<p class="form-row form-row-first">
-		<label for="password_1"><?php _e( 'Password (leave blank to leave unchanged)', BRETHEON_CHILD_DOMAIN ); ?></label>
-		<input type="password" class="input-text" name="password_1" id="password_1" />
-	</p>
-	<p class="form-row form-row-last">
-		<label for="password_2"><?php _e( 'Confirm new password', BRETHEON_CHILD_DOMAIN ); ?></label>
-		<input type="password" class="input-text" name="password_2" id="password_2" />
-	</p>
-
 	<?php
 	// additional account fields 
 	do_action( 'woocommerce_tailor_account_fields', $user );
@@ -46,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="clear"></div>
 
-	<p><input type="submit" class="button" name="save_account_details" value="<?php _e( 'Save changes', BRETHEON_CHILD_DOMAIN ); ?>" /></p>
+	<p><input type="submit" class="button" name="save_account_details" value="<?php _e( 'Save changes', WCT_DOMAIN ); ?>" /></p>
 
 	<?php wp_nonce_field( 'save_account_details' ); ?>
 	<input type="hidden" name="action" value="save_account_details" />
