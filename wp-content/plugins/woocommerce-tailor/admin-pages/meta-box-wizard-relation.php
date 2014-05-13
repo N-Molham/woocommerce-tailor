33b225a9bdf070a4cebea2ccc7bcb5ec8a9a2cad
nabeelmolham <nabeelmolham@localhost>
2014-05-13 21:50:01 +0200

    design wizard relation with products ( fabrics )

---
+++ wp-content/plugins/woocommerce-tailor/admin-pages/meta-box-wizard-relation.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Design Wizard Options
+ *
+ * @since 1.0
+ */
+
+// get filters
+if ( (float) phpversion() >= 5.4 )
+	$design_wizard_filters = WC_Tailor()->get_design_wizard_settings()['filters'];
+else
+	$design_wizard_filters = WC_Tailor()->get_design_wizard_settings( true )->filters;
+
+$filters_labels = WC_Tailor()->wizard_filters_labels();
+
+// panel start
+echo '<div class="panel woocommerce_options_panel">';
+
+// filters loop
+foreach ( $design_wizard_filters as $filter_name => $filter_data )
+{
+	// input value
+	$value = (array) get_post_meta( $post->ID, 'wc_filter_'. $filter_name );
+
+	// input layout
+	echo '<div class="options_group"><p class="form-field">';
+	echo '<label for="wc_tailor_', $filter_name ,'">', $filters_labels[$filter_name] ,'</label>';
+	echo '<select name="wc_tailor_filters[', $filter_name ,'][]" id="wc_tailor_', $filter_name ,'" multiple class="chosen">';
+	foreach ( $filter_data['options'] as $option_index => $option_name )
+	{
+		// option index (id)
+		echo '<option value="', $option_index ,'"';
+
+		// is selected
+		echo ( in_array( $option_index, $value ) ? ' selected' : '' );
+
+		// option name
+		echo '>', $option_name ,'</option>';
+	}
+	echo '</select></p></div>';
+}
+
+// panel end
+echo '</div>';
+
+?>
+<script>
+( function( window ) {
+	jQuery( function( $ ) {
+		$( '.chosen' ).chosen();
+	} );
+} )( window );
+</script>
---
+++ wp-content/plugins/woocommerce-tailor/includes/classes/meta-boxes.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * WP-Admin Meta Boxes
+ * 
+ * @since 1.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class WC_Tailor_Meta_Boxes
+{
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		// meta box actions
+		add_action( 'save_post', array( &$this, 'save_post_meta_box' ) );
+
+		// Setup meta boxes
+		add_action( 'add_meta_boxes', array( &$this, 'setup_meta_boxes' ) );
+	}
+
+	/**
+	 * 
+	 * 
+	 * @param int $post_id
+	 * @return void
+	 */
+	public function save_post_meta_box( $post_id )
+	{
+		// skip quick save
+		if ( !isset( $_POST['action'] ) || 'editpost' !== $_POST['action'] )
+			return;
+
+		switch ( get_post_type( $post_id ) )
+		{
+			case 'product':
+				// get filters
+				if ( (float) phpversion() >= 5.4 )
+					$design_wizard_filters = WC_Tailor()->get_design_wizard_settings()['filters'];
+				else
+					$design_wizard_filters = WC_Tailor()->get_design_wizard_settings( true )->filters;
+
+				// sanitize values
+				$selected_values = isset( $_POST['wc_tailor_filters'] ) ? (array) $_POST['wc_tailor_filters'] : array();
+				$selected_values = WC_Tailor_Utiles::array_map_recursive( $selected_values, 'intval' );
+
+				foreach ( $design_wizard_filters as $filter_name => $filter_data )
+				{
+					$meta_key = 'wc_filter_'. $filter_name;
+
+					// clean old values
+					delete_post_meta( $post_id, $meta_key );
+
+					// check filter
+					if ( !isset( $selected_values[$filter_name] ) )
+						continue;
+
+					// add values
+					foreach ( $selected_values[$filter_name] as $value )
+					{
+						// check value
+						if ( !isset( $filter_data['options'][$value] ) )
+							continue;
+
+						// add value to meta
+						add_post_meta( $post_id, $meta_key, $value );
+					}
+				}
+				break;
+		}
+	}
+
+	/**
+	 * Meta boxes setup
+	 * 
+	 * @param string $post_type
+	 * @hook add_meta_boxes
+	 * @return void
+	 */
+	public function setup_meta_boxes( $post_type = '' )
+	{
+		switch ( $post_type )
+		{
+			// product meta boxes
+			case 'product':
+				// enqueues
+				wp_enqueue_style( 'chosen-styles' );
+				wp_enqueue_style( 'wct-admin-style' );
+				wp_enqueue_script( 'chosen' );
+
+				// design wizard filters relation
+				add_meta_box( 'wc_tailor_wizard_relation', __( 'Product Filter Options', WCT_DOMAIN ), array( &$this, 'meta_box_layout' ), $post_type, 'normal', 'high' );
+				break;
+		}
+	}
+
+	/**
+	 * Load meta box layout file
+	 * 
+	 * @param WP_Post $post
+	 * @param array $meta_box
+	 * @return void
+	 */
+	public function meta_box_layout( $post, $meta_box )
+	{
+		// build page path
+		$page_path = WC_TAILOR_DIR .'admin-pages/meta-box-'. str_replace( array( 'wc_tailor_', '_' ), array( '', '-' ), $meta_box['id'] ) .'.php';
+
+		// check page existence
+		if ( file_exists( $page_path ) )
+		{
+			do_action( 'woocommerce_tailor_meta_box_before', $page_path, $meta_box['id'] );
+
+			include apply_filters( 'woocommerce_tailor_meta_box_path', $page_path, $meta_box['id'] );
+
+			do_action( 'woocommerce_tailor_meta_box_after', $page_path, $meta_box['id'] );
+		}
+		else
+			echo '<div class="error"><p><strong>'. __( 'Meta box not found.', WCT_DOMAIN ) .'</strong></p></div>';
+	}
+}
+
+
+
+
+
+
